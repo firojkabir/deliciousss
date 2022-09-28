47d990571e4169d8820b3195ea9295f6773cecf2
firojkabir <firoj.kabir@obcc.de>
2022-09-28 12:28:18 +0200

    Create search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Category from './components/Category.jsx';
+import Search from './components/Search.jsx';
 import Pages from './pages/Pages.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <Category />
       <Pages />
     </div>
