6496d460a2685110068484c895ff6f39721095cd
firojkabir <firoj.kabir@obcc.de>
2022-09-29 11:11:42 +0200

    Link to product details page g

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { GiKnifeFork } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import Category from './components/Category.jsx';
 import Search from './components/Search.jsx';
 import Pages from './pages/Pages.jsx';
@@ -6,11 +9,34 @@ import Pages from './pages/Pages.jsx';
 function App() {
   return (
     <div className="App">
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={'/'}>deliciousss</Logo>
+      </Nav>
       <Search />
       <Category />
       <Pages />
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`
+
 
 export default App;
